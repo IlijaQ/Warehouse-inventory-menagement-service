6b70558c0229685b574b5172a63b4d186fd5f880
Ilija Q <79657913+IlijaQ@users.noreply.github.com>
2024-10-11 20:11:14 +0200

    WarehouseApi: Add Context class

---
+++ API/WarehouseApi/Data/WarehouseContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WarehouseModels.Models;
+
+namespace WarehouseApi.Data
+{
+    public class WarehouseContext : DbContext
+    {
+        public WarehouseContext(DbContextOptions<WarehouseContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<ProductCategory>().HasKey(ck => new { ck.ProductId, ck.CategoryId });
+        }
+
+        public DbSet<WarehouseModels.Models.Product> Product { get; set; }
+        public DbSet<WarehouseModels.Models.Category> Category { get; set; }
+        public DbSet<WarehouseModels.Models.ProductCategory> ProductCategory { get; set; }
+    }
+}
