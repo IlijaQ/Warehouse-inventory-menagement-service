c2bad1dd3ff6e1b355d8f22caa6b487ad67f5c28
Ilija Q <79657913+IlijaQ@users.noreply.github.com>
2024-10-12 17:03:58 +0200

    WarehouseApi: Add Class for instantiating data transfer object, add FromDtoToProduct method

---
+++ API/WarehouseApi/DataTransferClasses/ProductData.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using WarehouseModels.Models;
+
+namespace WarehouseApi.DataTransferClasses
+{
+    public class ProductData
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public int StockQuantity { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public List<int> CheckedCategoryIds { get; set; }
+    }
+}
---
+++ API/WarehouseApi/Tools/TransferData.cs
@@ -0,0 +1,31 @@
+﻿using WarehouseModels.Models;
+using WarehouseApi.DataTransferClasses;
+
+namespace WarehouseApi.Tools
+{
+    public static class TransferData
+    {
+        public static Product FromDtoToProduct(ProductData data)
+        {
+            Product product = new Product();
+
+            product.ProductId = data.ProductId;
+            product.ProductName = data.ProductName;
+            product.Price = data.Price;
+            product.Description = data.Description;
+            product.StockQuantity = data.StockQuantity;
+            product.CreatedAt = data.CreatedAt;
+
+            IList<ProductCategory> productCategories = new List<ProductCategory>();
+
+            foreach (int cid in data.CheckedCategoryIds)
+            {
+                productCategories.Add(new ProductCategory { CategoryId = cid, ProductId = product.ProductId });
+            }
+
+            product.ProductCategory = productCategories;
+
+            return product;
+        }
+    }
+}
