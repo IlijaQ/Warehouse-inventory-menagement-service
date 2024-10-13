9c402e07efa39eb6620d2a2a26ed5d6be202cc57
Ilija Q <79657913+IlijaQ@users.noreply.github.com>
2024-10-13 04:10:14 +0200

    WarehouseUI: Create Product Create an Product Update UI

---
+++ UI/WarehouseUI/ProductCreateUpdate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WarehouseUI
+{
+    public partial class lblProductId : Form
+    {
+        public lblProductId()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
