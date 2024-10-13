121ed65c1110192738e8ababbdc2b6f0319dc27f
Ilija Q <79657913+IlijaQ@users.noreply.github.com>
2024-10-13 08:22:56 +0200

    WarehouseUI: Add data transfer classes

---
+++ UI/WarehouseUI/DataTransferClasses/SearchFilters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WarehouseApi.DataTransferClasses
+{
+    public class SearchFilters
+    {
+        public string Name { get; set; }
+        public DateTime? DateTimeAfter { get; set; }
+        public DateTime? DateTimeBefore { get; set; }
+        public decimal? PriceFrom { get; set; }
+        public decimal? PriceTo { get; set; }
+        public int? StockMoreThan { get; set; }
+        public int? StockLessThan { get; set; }
+    }
+}
